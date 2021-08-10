0dbeba5b1025405a3bc680a7f3d7f5be3b170195
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-08-10 08:49:17 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <p style={{ padding: "1rem",userSelect:"none" }}>
+        Easy web annotation
+      </p>
+    </div>
+  );
+};
+
+export default App;
